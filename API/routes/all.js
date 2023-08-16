c2b6cfd96028f7b25b892aed45cd3dae0c81c224
DennisBinford <dennis@binford.org>
2023-08-16 18:47:27 -0500

    named search parameters in main api path

--- API/routes/all.js
+++ API/routes/all.js
@@ -7,8 +7,10 @@ router.get("/", async (req, res) => {
         const page = req.page
         const limit = req.limit
 
-        let search = req.query.search || "";
-        req.query.search ? (search = req.query.search.split(",")) : (search = ["department"]);
+        const department = req.query.department || '';
+        const course = req.query.course || '';
+        const section = req.query.section || '';
+        const professor = req.query.professor || '';
         let sort = req.query.sort || "department";
         req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
 
@@ -18,23 +20,11 @@ router.get("/", async (req, res) => {
         sort[1] ? sortBy[sort[5]] = sort[5] : sortBy[sort[4]] = "asc";
         sort[1] ? sortBy[sort[7]] = sort[7] : sortBy[sort[6]] = "asc";
 
-        let findBy = [];
-        search[1] ? search0 = search[0] : search0 = 'department';
-        search[3] ? search1 = search[2] : search1 = 'course';
-        search[5] ? search2 = search[4] : search2 = 'section';
-        search[7] ? search3 = search[6] : search3 = 'professor';
-
-        search[1] ? findBy[0] = {[`${search0}`] : {$regex: search[1], $options: "i"}} : findBy = [{'department' : {$regex: '', $options: "i"}}]
-    
-        if (search[3]) {
-            findBy[1] = {[`${search1}`]: {$regex: search[3], $options: "i"}}
-        }
-        if (search[5]) {
-            findBy[2] = {[`${search2}`] : {$regex: search[5], $options: "i"}}
-        }
-        if (search[7]) {
-            findBy[3] = {[`${search3}`] : {$regex: search[7], $options: "i"}}
-        }
+        let findBy = [
+            {'department' : {$regex: department, $options: "i"}}, 
+            {'course' : {$regex: course, $options: "i"}},
+            {'section' : {$regex: section, $options: "i"}},
+            {'professor' : {$regex: professor, $options: "i"}}];
 
         const sections = await Section.find(
             {$and : findBy})
