f575177b208b2ff148910c66adced9fa0e69eb87
DennisBinford <dennis@binford.org>
2023-08-16 18:35:43 -0500

    patched page out of bounds false error trigger

--- tamugradeswebsite/src/App.jsx
+++ tamugradeswebsite/src/App.jsx
@@ -17,57 +17,62 @@ function App() {
   const [professorFilter, setProfessorFilter] = useState('');
 
     useEffect(()=>{
-        axios.get(`https://api.tamugrades.com/sections?limit=100&search=department,${departmentFilter},course,${courseFilter},section,${sectionFilter},professor,${professorFilter}`)
+        axios.get(`https://api.tamugrades.com?limit=1000&search=department,${departmentFilter},course,${courseFilter},section,${sectionFilter},professor,${professorFilter}`)
         .then(response => {
           setData(response.data.sections)
           setTotal(response.data.total)
         })
-        .catch(setData([]))
+        .catch(() => {
+          setData([])
+        })
     }, [departmentFilter, courseFilter, sectionFilter, professorFilter])
+  try {
+    return (
+      <div className="App">
+        <DataTable value={data} sortMode="multiple"
+        totalRecords={total}
+        paginator
+        rows={100}
+        rowsPerPageOptions={[5,10,25,50,100]}
+        showGridlines
+        removableSort
+        scrollable 
+        scrollHeight="1000px"
+        filterDisplay="row"
+        filter
+        onFilter={(e) => {
+          e.filters.department.value ? setDepartmentFilter(e.filters.department.value) : setDepartmentFilter('');
+          e.filters.course.value ? setCourseFilter(e.filters.course.value) : setCourseFilter('');
+          e.filters.section.value ? setDepartmentFilter(e.filters.section.value) : setSectionFilter('');
+          e.filters.professor.value ? setProfessorFilter(e.filters.professor.value) : setProfessorFilter('');
+          console.log(e.filters);
+          console.log(departmentFilter);
+          console.log(courseFilter);
+        }}
+        globalFilterFields={['department', 'course', 'section', 'professor']}
+        emptyMessage="No sections found."
+        >
+          <Column field="department" header="Department" sortable filter filterMatchMode="contains" filterMaxLength={4} filterPlaceholder="AERO"
+          />
+          <Column field="course" header="Course" sortable filter filterMatchMode="contains" filterMaxLength={3} filterPlaceholder="201"/>
+          <Column field="section" header="Section" sortable filter filterMatchMode="contains" filterMaxLength={3} filterPlaceholder="500"/>
+          <Column field="professor" header="Professor" sortable filter filterMatchMode="contains" filterMaxLength={100} filterPlaceholder="Smith"/>
+          <Column field="grades.0" header="A" sortable/>
+          <Column field="grades.1" header="B" sortable/>
+          <Column field="grades.2" header="C" sortable/>
+          <Column field="grades.3" header="D" sortable/>
+          <Column field="grades.4" header="F" sortable/>
+          <Column field="grades.8" header="Q" sortable/>
+          <Column field="semester" header="Semester" sortable/>
+          <Column field="year" header="Year" sortable/>
+        </DataTable>
 
-  return (
-    <div className="App">
-      <DataTable value={data} sortMode="multiple"
-      totalRecords={total}
-      paginator
-      rows={100}
-      rowsPerPageOptions={[5,10,25,50,100]}
-      showGridlines
-      removableSort
-      scrollable 
-      scrollHeight="1000px"
-      filterDisplay="row"
-      filter
-      onFilter={(e) => {
-        e.filters.department.value ? setDepartmentFilter(e.filters.department.value) : setDepartmentFilter('');
-        e.filters.course.value ? setCourseFilter(e.filters.course.value) : setCourseFilter('');
-        e.filters.section.value ? setDepartmentFilter(e.filters.section.value) : setSectionFilter('');
-        e.filters.professor.value ? setProfessorFilter(e.filters.professor.value) : setProfessorFilter('');
-        console.log(e.filters);
-        console.log(departmentFilter);
-        console.log(courseFilter);
-      }}
-      globalFilterFields={['department', 'course', 'section', 'professor']}
-      emptyMessage="No sections found."
-      >
-        <Column field="department" header="Department" sortable filter filterMatchMode="contains" filterMaxLength={4} filterPlaceholder="AERO"
-        />
-        <Column field="course" header="Course" sortable filter filterMatchMode="contains" filterMaxLength={3} filterPlaceholder="201"/>
-        <Column field="section" header="Section" sortable filter filterMatchMode="contains" filterMaxLength={3} filterPlaceholder="500"/>
-        <Column field="professor" header="Professor" sortable filter filterMatchMode="contains" filterMaxLength={100} filterPlaceholder="Smith"/>
-        <Column field="grades.0" header="A" sortable/>
-        <Column field="grades.1" header="B" sortable/>
-        <Column field="grades.2" header="C" sortable/>
-        <Column field="grades.3" header="D" sortable/>
-        <Column field="grades.4" header="F" sortable/>
-        <Column field="grades.8" header="Q" sortable/>
-        <Column field="semester" header="Semester" sortable/>
-        <Column field="year" header="Year" sortable/>
-      </DataTable>
 
-
-    </div>
-  )
+      </div>
+    )
+      } catch {
+        <h1>Refresh page, caught an error</h1>
+      }
 }
 
 
